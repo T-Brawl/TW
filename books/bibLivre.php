79e848e92ee983aaa5aa8e3d2091dab97a83ab67
T-Brawl <t-brawl@hotmail.fr>
2016-01-25 18:36:06 +0100

    Tabulation

--- books/bibLivre.php
+++ books/bibLivre.php
@@ -1,11 +1,11 @@
 <?php
 
 function imageLivres($tableau) {
-include_once 'Livre.class.php';
-echo '<table>';
-foreach($tableau as $tab)
-$tab->imageTR();
-echo '</table>';
+  include_once 'Livre.class.php';
+  echo '<table>';
+  foreach($tableau as $tab)
+    $tab->imageTR();
+  echo '</table>';
 }
 
 ?>
